ee996efe661d02b0d45ba969f4d16198ba2d117e
darwin_quiroz <darquiroz@gmail.com>
2020-06-20 20:25:04 -0500

    Remover tabla pivote del resultado

--- app/Product.php
+++ app/Product.php
@@ -14,6 +14,7 @@ class Product extends Model
 
     protected $fillable = ['name', 'description', 'quantity', 'status', 'image', 'seller_id'];
     protected $date = ['deleted_at'];
+    protected $hidden = ['pivot'];
 
     public function categories()
     {
