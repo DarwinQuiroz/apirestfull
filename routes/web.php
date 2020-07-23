28e41d912bc1c1dc135691ef70bff61a14c38d6f
darwin_quiroz <darquiroz@gmail.com>
2020-07-22 20:38:31 -0500

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,7 @@
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-})->middleware('guest');
+Route::view('/', 'welcome')->middleware('guest');
 
 Auth::routes(['register' => false, 'reset' => false]);
 
