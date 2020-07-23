eba27f2b3af3b5f10c3ce883fd9143406f77f6bb
darwin_quiroz <darquiroz@gmail.com>
2020-07-22 20:31:50 -0500

    Login scaffold

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,13 @@
 |
 */
 
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Route;
+
 Route::get('/', function () {
     return view('welcome');
-});
+})->middleware('guest');
+
+Auth::routes(['register' => false, 'reset' => false]);
+
+Route::get('/home', 'HomeController@index')->name('home');
